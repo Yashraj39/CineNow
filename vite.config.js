9d93541d83b40c9585774dd697ca949a86549987
Yashraj <parmaryashraj068@gmail.com>
2025-09-27 00:43:49 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/CineNow/', 
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
